1a064cfcf2d38a3996ba01e60d54a2714173e79a
MartirosCode <161162558+MartirosCode@users.noreply.github.com>
2024-03-18 18:51:14 +0300

    Add files via upload

--- start2.cpp
+++ start2.cpp
@@ -1,91 +1,21 @@
 #include <iostream>
 using namespace std;
-//1
-int main() {
-    setlocale(LC_ALL, "rus");
-    int number;
-    cout << "Введите число:";
-    cin >> number;
+int main(){
+int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+int n = sizeof(arr) / sizeof(arr[0]);
 
-    int countDigits = 0;
-    int sumDigits = 0;
-    int countZeros = 0;
-
-    int temp = number;
-    while (temp != 0) {
-        countDigits++;
-        sumDigits += temp % 10;
-        temp /= 10;
-    }
-    double avarage = static_cast<double>(sumDigits) / countDigits;
-
-    temp = number;
-    while (temp != 0) {
-        if (temp % 10 == 0) {
-            countZeros++;
-
-
-        }
-        temp /= 10;
-
-    }
-    cout << "Кол-во цифр:" << countDigits << endl;
-    cout << "Сумма цифр:" << sumDigits << endl;
-    cout << "Среднее арифметическое:" << avarage << endl;
-    cout << "Кол-во нулей:" << countZeros << endl;
-
-    //2
-    int cellSize;
-    cout << "Введите размер клетки:\n";
-    cin >> cellSize;
-    for (int i = 0; i < 8; i++)
-    {
-        if (i % 2 == 0)
-        {
-            for (int m = 0; m < cellSize; m++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    if (j % 2 == 0)
-                    {
-                        for (int k = 0; k < cellSize; k++)
-                        {
-                            cout << "*";
-                        }
-                    }
-                    else
-                    {
-                        for (int k = 0; k < cellSize; k++)
-                        {
-                            cout << "-";
-                        }
-                    }
-                }
-                cout << "\n";
-            }
-        }
-        else {
-            for (int m = 0; m < cellSize; m++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    if (j % 2 == 0)
-                    {
-                        for (int k = 0; k < cellSize; k++)
-                        {
-                            cout << "-";
-                        }
-                    }
-                    else
-                    {
-                        for (int k = 0; k < cellSize; k++)
-                        {
-                            cout << "*";
-                        }
-                    }
-                }
-                cout << "\n";
-            }
+for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+            std::swap(arr[j], arr[j + 1]);
         }
     }
+}
+
+for (int i = 0; i < n; i++) {
+    std::cout << arr[i] << " ";
+}
+std::cout << std::endl;
+
+return 0;
 }
